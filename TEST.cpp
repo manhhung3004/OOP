59436bdd6d03cdee067ebb0e339b7c0853ff9dcc
Nguyễn Đặng Thanh Huy <102576374+21522149@users.noreply.github.com>
2022-04-19 19:52:27 +0700

    Update TEST.cpp

--- TEST.cpp
+++ TEST.cpp
@@ -6,7 +6,7 @@ int main()
     int n; cin >> n;
     for(int i =0 ; i < n ; i++)
     {
-            cout <<"Tao la mot con lon"<<endl;
+           cout <<" chay kpi giao dien den het tuan nay nha con lon"<<endl;
     }
     system("pause");
     return 0;
