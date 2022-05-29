58b4492a34ecf45c242a871409090b5cf626de8b
Nguyễn Đặng Thanh Huy <102576374+21522149@users.noreply.github.com>
2022-05-29 09:17:52 +0700

    Menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,25 +1,33 @@
-#include"Music_composser.h"
+#include"Music_Composser.h"
+using namespace std;
 
-void Menu(vector<NOTE*>& Song) {
-    int choice1 = 0;
+void Menu(vector <NOTE*>& Song)
+{
+	int choice1 = 0;
 
-    do {
-        system("cls");
-        cout << "\t\t\t\t ______________________________________________________________\n";
-        cout << "\t\t\t\t|                        ****** MENU *****                     |\n";
-        cout << "\t\t\t\t|                       1/ Tao bai hat moi.                    |\n";
-        cout << "\t\t\t\t|                       2/ Phat bai hat co san.                |\n";
-        cout << "\t\t\t\t|                       3/ Thoat chuong trinh.                 |\n";
-        cout << "\t\t\t\t --------------------------------------------------------------\n";
-        cin >> choice1;
-        switch (choice1) {
-        case 1:
-            system("cls");
-            NewSong(Song);
-            break;
-        case 2:
-            SavedSong();
-            break;
-        }
-    } while (choice1 != 3);
+	do {
+		system("cls");
+		cout << " ______________________________________________________________\n";
+		cout << "|                          MENU                                |\n";
+		cout << "|  1/ Tao ban nhac moi.                                        |\n";
+		cout << "|  2/ Phat ban nhac co san.                                    |\n";
+		cout << "|  3/ Thoat chuong trinh.                                      |\n";
+		cout << " --------------------------------------------------------------\n";
+
+		while (!(cin >> choice1) || choice1 > 3 || choice1 < 1) {
+			cin.clear();
+			cin.ignore(256, '\n');
+			cout << "Gia tri khong hop le! Hay nhap lai: \n";
+		}
+
+		switch (choice1) {
+		case 1:
+			system("cls");
+			NewSong(Song);
+			break;
+		case 2:
+			SavedSong(Song);
+			break;
+		}
+	} while (choice1 != 3);
 }
